dcdab03d92e4567ccf9d16796cfe5ada5efdf394
kammradt <vinicius.kammradt1@gmail.com>
2019-04-23 10:56:41 -0300

    :building_construction: - Creating Toolbar component.

--- album-bootstrap-template/src/App.vue
+++ album-bootstrap-template/src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div>
-    <v-layout row wrap>
-      <v-toolbar
-        dark
-        flat
-        height="60"
-        color="#343A40"
-      >
-        <v-btn flat :ripple='false'>
-          <v-icon color="grey" left>photo_camera</v-icon>
-          <span class="text-capitalize headline font-weight-medium">Album</span>
-        </v-btn>
-        <v-spacer/>
-        <v-btn flat :ripple='false'>
-          <v-icon large color="grey">menu</v-icon>
-        </v-btn>
-      </v-toolbar>
-    </v-layout>
-  </div>
+  <v-app>
+    <SuperiorToolbar />
+  </v-app>
 </template>
 
 <script>
-
+import SuperiorToolbar from '@/components/SuperiorToolbar'
 export default {
   name: 'app',
+  components: {
+    SuperiorToolbar
+  }
 }
 </script>
