465d8a361de78f0bebf2aa8d40bad9ef0b9022d4
kammradt <vinicius.kammradt1@gmail.com>
2019-04-23 23:59:49 -0300

    :sparkles: - PrimaryAction now has its own Popup component finished.

--- album-bootstrap-template/src/components/Popup.vue
+++ album-bootstrap-template/src/components/Popup.vue
@@ -3,8 +3,29 @@
     <v-btn flat slot="activator" class="secondary">Primary action</v-btn>
     <v-card>
       <v-card-title>
-        <h2>Hello!</h2>
+        <span class="font-weight-light display-1">Hello, this is an example!!</span>
       </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field
+            label="Title"
+            prepend-icon="folder"
+            color="pink darken-1"
+          />
+          <v-text-field
+            label="Description"
+            prepend-icon="edit"
+            color="pink darken-1"
+            textarea
+          />
+          <v-btn
+            dark
+            color="secondary"
+            class="mx-0 mt-3"
+            v-text="'Confirm'"
+          />
+        </v-form>
+      </v-card-text>
     </v-card>
   </v-dialog>
 </template>
