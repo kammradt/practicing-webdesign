e480053cf3c2cb3f188a2d2dfbc9db1bddbcd0cc
kammradt <vinicius.kammradt1@gmail.com>
2019-04-26 11:35:56 -0300

    :construction: - Creating ThumbnailGrid component (Text and Image).

--- album-bootstrap-template/src/components/ThumbnailGrid.vue
+++ album-bootstrap-template/src/components/ThumbnailGrid.vue
@@ -1,9 +1,40 @@
 <template>
   <v-content>
-    <v-layout row wrap red>
-      <v-flex xs12>
-        teste
-      </v-flex>
-    </v-layout>
+    <v-flex xs12 grey lighten-1>
+      <v-container grid-list-md>
+        <v-layout row wrap>
+          <v-flex v-for="card in cards" :key="card.title" xs12 md4 pa-3>
+            <v-card>
+              <v-flex xs12>
+                <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"/>
+              </v-flex>
+              <v-card-text>
+                {{ card.content }}
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-flex>
   </v-content>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      cards: [
+        {title: 'My title 1', content: 'Sed ut. Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore.'},
+        {title: 'My title 2', content: 'Sed ut. Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore.'},
+        {title: 'My title 3', content: 'Sed ut. Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore.'},
+        {title: 'My title 4', content: 'Sed ut. Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore.'},
+        {title: 'My title 5', content: 'Sed ut. Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore.'},
+        {title: 'My title 6', content: 'Sed ut. Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore.'},
+        {title: 'My title 7', content: 'Sed ut. Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore.'},
+        {title: 'My title 8', content: 'Sed ut. Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore.'},
+        {title: 'My title 9', content: 'Sed ut. Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore.'},
+      ]
+    }
+  }
+}
+</script>
