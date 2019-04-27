ab64e9273ee5fb291cd55c145ed7d08b8789cae7
kammradt <vinicius.kammradt1@gmail.com>
2019-04-27 12:36:55 -0300

    :sparkles: - Footer component created.

--- album-bootstrap-template/src/App.vue
+++ album-bootstrap-template/src/App.vue
@@ -4,6 +4,7 @@
       <SuperiorToolbar />
       <MiddleText />
       <ThumbnailGrid />
+      <Footer />
     </v-content>
   </v-app>
 </template>
@@ -12,13 +13,15 @@
 import SuperiorToolbar from '@/components/SuperiorToolbar'
 import MiddleText from '@/components/MiddleText'
 import ThumbnailGrid from '@/components/ThumbnailGrid'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'app',
   components: {
     SuperiorToolbar,
     MiddleText,
-    ThumbnailGrid
+    ThumbnailGrid,
+    Footer
   }
 }
 </script>
---
+++ album-bootstrap-template/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-content>
+    <v-layout row wrap my-4>
+      <v-flex xs9>
+        <p class="px-3 caption grey--text">
+          Album example is © Bootstrap, but please download and customize it for yourself!
+          New to Bootstrap? <a>Visit the homepage</a> or read our <a>getting started guide.</a>
+        </p>
+      </v-flex>
+      <v-flex xs3 text-xs-right>
+        <p class="px-3 caption grey--text">
+          <a>Back to top</a>
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-content>
+</template>
