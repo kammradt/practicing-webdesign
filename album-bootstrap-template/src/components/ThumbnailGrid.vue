07f2cec0e17c76df3051627b7a653369e5870522
kammradt <vinicius.kammradt1@gmail.com>
2019-04-27 12:17:27 -0300

    :iphone: - Fixing image sizes.

--- album-bootstrap-template/src/components/ThumbnailGrid.vue
+++ album-bootstrap-template/src/components/ThumbnailGrid.vue
@@ -3,7 +3,7 @@
     <v-flex xs12 grey lighten-1>
       <v-container grid-list-md>
         <v-layout row wrap>
-          <v-flex v-for="card in cards" :key="card.title" xs12 md4 pa-3>
+          <v-flex v-for="card in cards" :key="card.title" xs12 sm4 pa-3>
             <v-card>
               <v-flex xs12>
                 <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"/>
